c3d364ad1fa9923a4be32415ec647aeb29cd04fb
Michael Lynch <git@mtlynch.io>
2021-02-20 22:05:13 -0500

    Switch to new domain name

--- handlers/index.go
+++ handlers/index.go
@@ -17,8 +17,7 @@ func (s defaultServer) serveIndexPage() http.HandlerFunc {
 		indexTemplate := template.Must(template.New(indexFilename).
 			ParseFiles(path.Join(viewsRootDir, indexFilename)))
 		if err := indexTemplate.ExecuteTemplate(w, indexFilename, page{
-			//BackendBaseURL: "https://logs.tinypilotkvm.com",
-			BackendBaseURL: "https://logpaste-fqju5wfryq-ue.a.run.app",
+			BackendBaseURL: "https://logs.tinypilotkvm.com",
 		}); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
