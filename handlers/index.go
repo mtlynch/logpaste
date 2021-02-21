8f872daaa84dbeea34e58021066a8fbe41306d2b
Michael Lynch <git@mtlynch.io>
2021-02-20 21:52:53 -0500

    Add demo application at root route

--- handlers/index.go
+++ handlers/index.go
@@ -12,12 +12,13 @@ const viewsRootDir = "./views"
 func (s defaultServer) serveIndexPage() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		type page struct {
-			Title string
+			BackendBaseURL string
 		}
 		indexTemplate := template.Must(template.New(indexFilename).
 			ParseFiles(path.Join(viewsRootDir, indexFilename)))
 		if err := indexTemplate.ExecuteTemplate(w, indexFilename, page{
-			Title: "LogPaste",
+			//BackendBaseURL: "https://logs.tinypilotkvm.com",
+			BackendBaseURL: "https://logpaste-fqju5wfryq-ue.a.run.app",
 		}); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
