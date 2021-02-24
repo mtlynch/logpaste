b79161c8c98367cff1f70f62ac02248a34cf0d0e
Michael Lynch <mtlynch@users.noreply.github.com>
2021-02-23 20:31:50 -0500

    Return 404 for favicon.ico route (#9)

--- handlers/routes.go
+++ handlers/routes.go
@@ -4,7 +4,14 @@ import (
 	"net/http"
 )
 
+func notFound() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Resource not found", http.StatusNotFound)
+	}
+}
+
 func (s *defaultServer) routes() {
+	s.router.HandleFunc("/favicon.ico", notFound()).Methods(http.MethodGet)
 	s.router.PathPrefix("/{id}").HandlerFunc(s.pasteGet()).Methods(http.MethodGet)
 	s.router.PathPrefix("/").HandlerFunc(s.pasteOptions()).Methods(http.MethodOptions)
 	s.router.PathPrefix("/").HandlerFunc(s.pastePut()).Methods(http.MethodPut)
