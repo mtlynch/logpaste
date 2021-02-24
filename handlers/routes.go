5dfee7da3536bd7ec40e8b6bc5dae16f5849d634
Michael Lynch <mtlynch@users.noreply.github.com>
2021-02-23 21:08:01 -0500

    Add CSS stylesheet and adjust UI (#13)

--- handlers/routes.go
+++ handlers/routes.go
@@ -12,6 +12,7 @@ func notFound() http.HandlerFunc {
 
 func (s *defaultServer) routes() {
 	s.router.HandleFunc("/favicon.ico", notFound()).Methods(http.MethodGet)
+	s.router.PathPrefix("/css/").HandlerFunc(s.serveStaticResource()).Methods(http.MethodGet)
 	s.router.PathPrefix("/js/").HandlerFunc(s.serveStaticResource()).Methods(http.MethodGet)
 	s.router.PathPrefix("/{id}").HandlerFunc(s.pasteGet()).Methods(http.MethodGet)
 	s.router.PathPrefix("/").HandlerFunc(s.pasteOptions()).Methods(http.MethodOptions)
