d7a807455d78b8a7d1569f24cb2349b0124cb142
Michael Lynch <mtlynch@users.noreply.github.com>
2022-08-19 09:59:54 -0400

    Adding shellcheck to CI (#166)
    * Adding shellcheck to CI
    
    * Fix bash lint
    
    * Fix shell lint
    
    * Remove exec
    
    * Fix shell lint

--- e2e/wait-for-sftp.bash
+++ e2e/wait-for-sftp.bash
@@ -7,7 +7,7 @@ shift
 SFTP_PORT="$1"
 shift
 
-timeout 15 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/$0/$1; do sleep 1; done' "${SFTP_HOST}" "${SFTP_PORT}"
+timeout 15 bash -c "until printf '' 2>>/dev/null >>/dev/tcp/${SFTP_HOST}/${SFTP_PORT}; do sleep 1; done"
 
 >&2 echo "sftp is up - executing command"
 
