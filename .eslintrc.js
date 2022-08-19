2e66d88054f16862610936c31455bd0f367608cc
Michael Lynch <git@mtlynch.io>
2022-08-19 10:24:50 -0400

    Add ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+      node: true,
+      browser: true,
+      es2022: true,
+    },
+    extends: ["plugin:cypress/recommended", "eslint:recommended"],
+    rules: {
+      // This will produce an error for console.log or console.warn in production
+      // and a warning in development console.error will not produce an error or
+      // warning https://eslint.org/docs/rules/no-console#options
+      "no-console": [
+        process.env.NODE_ENV === "production" ? "error" : "warn",
+        { allow: ["error"] },
+      ],
+    },
+    ignorePatterns: ["static/third-party/**/*.js"],
+  };
