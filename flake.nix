a56f976f7ef56d10a66d37e6bfc3d704947ca2f1
Michael Lynch <mtlynch@users.noreply.github.com>
2023-09-28 21:35:48 -0400

    Add shellcheck to Nix flake (#182)

--- flake.nix
+++ flake.nix
@@ -16,6 +16,12 @@
         url = "https://github.com/NixOS/nixpkgs/archive/81094ccd6a0aa13ed176c815a60c4e25b49f072d.tar.gz";
         sha256 = "1h7bx9fnda66g651l8mymjzyb0y9km5b9sgwy2dla3dz4pvbk1zd";
       }) {inherit system; };
+
+      pkgs_for_shellcheck = import (builtins.fetchTarball {
+        # 0.9.0 release
+        url = "https://github.com/NixOS/nixpkgs/archive/8b5ab8341e33322e5b66fb46ce23d724050f6606.tar.gz";
+        sha256 = "05ynih3wc7shg324p7icz21qx71ckivzdhkgf5xcvdz6a407v53h";
+      }) {inherit system; };
     in
     {
       devShells.default = pkgs_for_go.mkShell.override { stdenv = pkgs_for_go.pkgsStatic.stdenv; } {
@@ -23,9 +29,11 @@
           gopls
           gotools
           pkgs_for_go.go
+          pkgs_for_shellcheck.shellcheck
         ];
 
         shellHook = ''
+          echo "shellcheck" "$(shellcheck --version | grep '^version:')"
           go version
         '';
       };
