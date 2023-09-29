082b5e898c227b21e7ed2331ad24cf63244adf63
Michael Lynch <mtlynch@users.noreply.github.com>
2023-09-28 21:45:49 -0400

    Add sqlfluff to Nix flake (#183)

--- flake.nix
+++ flake.nix
@@ -22,6 +22,12 @@
         url = "https://github.com/NixOS/nixpkgs/archive/8b5ab8341e33322e5b66fb46ce23d724050f6606.tar.gz";
         sha256 = "05ynih3wc7shg324p7icz21qx71ckivzdhkgf5xcvdz6a407v53h";
       }) {inherit system; };
+
+      pkgs_for_sqlfluff = import (builtins.fetchTarball {
+        # 1.2.1 release
+        url = "https://github.com/NixOS/nixpkgs/archive/7cf5ccf1cdb2ba5f08f0ac29fc3d04b0b59a07e4.tar.gz";
+        sha256 = "0wfaqjpi7bip86r2piqigqna1fx3m1d9riak4l3rm54lyjxprlpi";
+      }) {inherit system; };
     in
     {
       devShells.default = pkgs_for_go.mkShell.override { stdenv = pkgs_for_go.pkgsStatic.stdenv; } {
@@ -30,10 +36,12 @@
           gotools
           pkgs_for_go.go
           pkgs_for_shellcheck.shellcheck
+          pkgs_for_sqlfluff.sqlfluff
         ];
 
         shellHook = ''
           echo "shellcheck" "$(shellcheck --version | grep '^version:')"
+          sqlfluff --version
           go version
         '';
       };
