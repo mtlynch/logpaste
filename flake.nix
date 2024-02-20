a8220849dec706cfa9123cd17ec7e5cddd8e2d89
Michael Lynch <git@mtlynch.io>
2024-02-20 17:04:16 -0500

    Add flyctl to Nix flake

--- flake.nix
+++ flake.nix
@@ -15,15 +15,19 @@
 
     # 1.2.1 release
     sqlfluff_dep.url = "github:NixOS/nixpkgs/7cf5ccf1cdb2ba5f08f0ac29fc3d04b0b59a07e4";
+
+    # 0.1.147 release
+    flyctl_dep.url = "github:NixOS/nixpkgs/0a254180b4cad6be45aa46dce896bdb8db5d2930";
   };
 
-  outputs = { self, flake-utils, go_dep, nodejs_dep, shellcheck_dep, sqlfluff_dep }@inputs :
+  outputs = { self, flake-utils, go_dep, nodejs_dep, shellcheck_dep, sqlfluff_dep, flyctl_dep }@inputs :
     flake-utils.lib.eachDefaultSystem (system:
     let
       go_dep = inputs.go_dep.legacyPackages.${system};
       nodejs_dep = inputs.nodejs_dep.legacyPackages.${system};
       shellcheck_dep = inputs.shellcheck_dep.legacyPackages.${system};
       sqlfluff_dep = inputs.sqlfluff_dep.legacyPackages.${system};
+      flyctl_dep = inputs.flyctl_dep.legacyPackages.${system};
     in
     {
       devShells.default = go_dep.mkShell.override { stdenv = go_dep.pkgsStatic.stdenv; } {
@@ -34,11 +38,13 @@
           nodejs_dep.nodejs_20
           shellcheck_dep.shellcheck
           sqlfluff_dep.sqlfluff
+          flyctl_dep.flyctl
         ];
 
         shellHook = ''
           echo "shellcheck" "$(shellcheck --version | grep '^version:')"
           sqlfluff --version
+          fly version | cut -d ' ' -f 1-3
           echo "node" "$(node --version)"
           echo "npm" "$(npm --version)"
           go version
