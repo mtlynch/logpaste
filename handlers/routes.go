c5fcfa70d0fc4622525bb15c1b2b354d20515cb0
Michael Lynch <git@mtlynch.io>
2025-04-24 19:46:21 -0400

    work in progress

--- handlers/routes.go
+++ handlers/routes.go
@@ -15,9 +15,9 @@ func (s *defaultServer) routes() {
 	s.router.PathPrefix("/css/").HandlerFunc(serveStaticResource()).Methods(http.MethodGet)
 	s.router.PathPrefix("/js/").HandlerFunc(serveStaticResource()).Methods(http.MethodGet)
 	s.router.PathPrefix("/third-party/").HandlerFunc(serveStaticResource()).Methods(http.MethodGet)
-	//s.router.Path("/").HandlerFunc(serveStaticResource()).Methods(http.MethodGet)
 	s.router.PathPrefix("/{id}").HandlerFunc(s.pasteGet()).Methods(http.MethodGet)
 	s.router.PathPrefix("/").HandlerFunc(s.pasteOptions()).Methods(http.MethodOptions)
 	s.router.PathPrefix("/").Handler(s.ipRateLimiter.Limit(s.pastePut())).Methods(http.MethodPut)
 	s.router.PathPrefix("/").Handler(s.ipRateLimiter.Limit(s.pastePost())).Methods(http.MethodPost)
+	s.router.PathPrefix("/").HandlerFunc(s.serveIndexPage()).Methods(http.MethodGet)
 }
